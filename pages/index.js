61e06f1310dd6c2bc5c5410794365d739b5a4713
TarcioCarvalho <tarciodevlead@gmail.com>
2023-10-03 12:12:02 -0300

    Title phrase of the page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home () {
     return (
         <>
-            <h1>O medo de arriscar nos impede de conseguirmos grandes oportunidades</h1>
+            <h1>O medo de errar retira oportunidades!</h1>
         </>
     )
 }
